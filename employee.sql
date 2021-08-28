2716d4de7f38534e3123596120956ea1d89b3308
venu67776 <83317608+venu67776@users.noreply.github.com>
2021-08-28 18:50:09 +0530

    new

--- employee.sql
+++ employee.sql
@@ -2,14 +2,14 @@ create database EmployeeDb;
 
 use EmployeeDb;
 
-create table department
+create table Department
 (
-  departmentId int not null,
-  departmentName varchar(100) not null,
+  DepartmentId int not null,
+  DepartmentName varchar(100) not null,
   primary key (departmentId)
 );
 
-insert into department values 
+insert into Department values 
 (1, 'sales' ), 
 (2, 'finance' ),
 (3, 'IT' ),
@@ -17,16 +17,17 @@ insert into department values
 
 
 
-create table employee 
+create table Employee 
 (
   EmployeeId int not null,
   EmployeeName varchar(100) not null,
-  Dateofjoining date,
+  DateofJoining date,
   Department varchar(500) not null,
-  Photofilename varchar(500),
+  PhotoFileName varchar(500),
   primary key (EmployeeId)
 );
 
-insert into employee values 
+insert into Employee values  
 (1, 'Clark','2020-06-12',sales,'anaonmous.png'),
-(6, 'raja', '2020-06-12',HR,'raja.png');
+(2, 'raja', '2020-06-12',HR,'raja.png');
+
